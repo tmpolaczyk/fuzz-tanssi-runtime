c6dc2e0d0d1b54beeb44d94e9a294bb7e61a6524
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2023-11-22 19:35:22 +0100

    Better run_coverage.py script, and coverage template

---
+++ fuzz/fuzz_targets/coverage.rs
@@ -0,0 +1,65 @@
+const FUZZ_TARGET_NAME: &str = "fuzz_raw";
+
+fn main() {
+    use {
+        indicatif::{ParallelProgressIterator, ProgressStyle},
+        rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator},
+        std::collections::HashSet,
+    };
+
+    let mut corpus_path = env!("CARGO_MANIFEST_DIR").to_string();
+    corpus_path.push_str("/corpus/");
+    corpus_path.push_str(FUZZ_TARGET_NAME);
+    println!("corpus path: {:?}", corpus_path);
+    let mut seen_paths = HashSet::new();
+    let mut i = 0;
+    // Process new entries until we catch up with the fuzzer
+    // First iteration will process all entries
+    // Second iteration will only process new entries
+    loop {
+        i += 1;
+        let entries: Vec<_> = std::fs::read_dir(&corpus_path)
+            .unwrap_or_else(|e| {
+                panic!(
+                    "Failed to read corpus path {:?}, error: {:?}",
+                    corpus_path, e
+                )
+            })
+            .filter_map(|entry| {
+                if entry.is_ok() && seen_paths.contains(&entry.as_ref().unwrap().path()) {
+                    None
+                } else {
+                    Some(entry)
+                }
+            })
+            .collect();
+        println!("iteration {} new entries: {}", i, entries.len());
+
+        if entries.is_empty() {
+            break;
+        }
+
+        let style = ProgressStyle::with_template("[{elapsed_precise}] ETA: [{eta_precise}] {bar:40.cyan/blue} {percent}% {pos:>7}/{len:7} {msg}").unwrap();
+        entries
+            .par_iter()
+            .progress_with_style(style)
+            .for_each(|entry| {
+                if entry.is_err() {
+                    return;
+                }
+                let entry = entry.as_ref().unwrap();
+                let data = std::fs::read(entry.path());
+                if data.is_err() {
+                    return;
+                }
+                let data = data.unwrap();
+                fuzz_main(&data);
+            });
+
+        seen_paths.extend(
+            entries
+                .into_iter()
+                .filter_map(|entry| entry.ok().map(|entry| entry.path())),
+        );
+    }
+}
--- fuzz/run_coverage.py
+++ fuzz/run_coverage.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
+import os
+import subprocess
 
-def merge_rust_files_optimized(file1, file2, output_file):
+def setup_coverage_target(target_name):
+    """
+    Sets up the coverage target by running a cargo fuzz command.
+    Ignores failures and suppresses all output.
+    """
+    target_coverage_name = f"{target_name}_coverage"
+    subprocess.run(
+        ["cargo", "fuzz", "add", target_coverage_name],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
+def write_coverage_target(target_name, file1, file2, output_file):
+    """
+    Merges two Rust files into a new file with specific modifications.
+    """
     # Define the header based on the file names
     header = [
-        f"//! Generate coverage for the `fuzz_raw` target.\n",
+        f"//! Generate coverage for the `{target_name}` target.\n",
         f"//! Warning: this file has been automatically generated using the `run_coverage.py` script\n",
         f"//! from the contents of `{file1}` and `{file2}`.\n",
         "\n"
@@ -16,7 +33,7 @@ def merge_rust_files_optimized(file1, file2, output_file):
     for i, line in enumerate(file1_contents):
         stripped_line = line.strip()
         if stripped_line.startswith("const FUZZ_TARGET_NAME: &str"):
-            file1_contents[i] = 'const FUZZ_TARGET_NAME: &str = "fuzz_raw";\n'
+            file1_contents[i] = f'const FUZZ_TARGET_NAME: &str = "{target_name}";\n'
             break
 
     # Read and modify the contents of the second file
@@ -36,8 +53,54 @@ def merge_rust_files_optimized(file1, file2, output_file):
     if final_content[-1] == "\n":
         final_content.pop()
 
-    with open(output_file, 'w') as f:
-        f.writelines(final_content)
+    # Combine all lines in final_content into a single string
+    final_content_join = "".join(final_content)
 
-# Example usage
-merge_rust_files_optimized('fuzz_targets/coverage.rs', 'fuzz_targets/fuzz_raw.rs', 'fuzz_targets/fuzz_raw_coverage.rs')
+    # Check if the output file already has this content
+    # This avoids triggering a useless cargo build
+    try:
+        with open(output_file, 'r') as f:
+            existing_content_join = f.read()
+
+        # Using difflib to find differences
+        if existing_content_join == final_content_join:
+            return  # No need to write if the content is the same
+    except FileNotFoundError:
+        print(f"{output_file} not found. A new file will be created.")
+
+    # Write to the output file
+    with open(output_file, 'w') as f:
+        f.write(final_content_join)
+        print(f"Updated {output_file}")
+
+def build_coverage_binary(target_name):
+    """
+    Builds the coverage binary by running a cargo run command with specific RUSTFLAGS.
+    """
+    env = os.environ.copy()
+    env["RUSTFLAGS"] = "--cfg fuzzing --cfg coverage -C opt-level=3 -C target-cpu=native -C instrument-coverage"
+    env["LLVM_PROFILE_FILE"] = "/dev/null"
+    subprocess.run(["cargo", "build", "--bin", f"{target_name}_coverage"], env=env)
+
+def execute_coverage_binary(target_name):
+    """
+    Executes the 'fuzz_raw_coverage' binary located in the 'target/debug' directory.
+    """
+    try:
+        env = os.environ.copy()
+        # TODO: use absolute path
+        env["LLVM_PROFILE_FILE"] = "proffiles/default_%m_%p.profraw"
+        subprocess.run([f"./target/debug/{target_name}_coverage"])
+    except Exception as e:
+        print(f"Failed to execute '{target_name}_coverage': {e}")
+
+def main():
+    target_name = "fuzz_raw"
+    setup_coverage_target(target_name)
+    write_coverage_target(target_name, 'fuzz_targets/coverage.rs', f"fuzz_targets/{target_name}.rs", f"fuzz_targets/{target_name}_coverage.rs")
+    build_coverage_binary(target_name)
+    execute_coverage_binary(target_name)
+
+if __name__ == "__main__":
+    main()
+
