5d1c969665dc230bb29cd41a5b13d6700243414e
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2025-08-07 15:19:25 +0200

    Disable custom fuzz mutators

--- fuzzers/fuzz-dancelight/fuzz/fuzz_targets/fuzz_live_oneblock.rs
+++ fuzzers/fuzz-dancelight/fuzz/fuzz_targets/fuzz_live_oneblock.rs
@@ -4,6 +4,7 @@ use fuzz_dancelight::*;
 
 libfuzzer_sys::fuzz_target!(init: fuzz_init::<FuzzLiveOneblock>(), |data: &[u8]| fuzz_live_oneblock::<FuzzLiveOneblock>(data));
 
+/*
 libfuzzer_sys::fuzz_crossover!(|data1: &[u8], data2: &[u8], out: &mut [u8], seed: u32| {
     fuzz_crossover_extr_or_pseudo(data1, data2, out, seed)
 });
@@ -11,3 +12,4 @@ libfuzzer_sys::fuzz_crossover!(|data1: &[u8], data2: &[u8], out: &mut [u8], seed
 libfuzzer_sys::fuzz_mutator!(|data: &mut [u8], size: usize, max_size: usize, seed: u32| {
     fuzz_mutator_extr_or_pseudo(data, size, max_size, seed)
 });
+*/
