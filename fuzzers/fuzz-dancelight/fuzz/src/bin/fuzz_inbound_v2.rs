600f5c266c6ee0c956fb17e10afe073b21a27410
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2025-12-15 17:53:59 +0100

    newer nightly

---
+++ fuzzers/fuzz-dancelight/fuzz/src/bin/fuzz_inbound_v2.rs
@@ -0,0 +1,17 @@
+#![no_main]
+
+//! Tanssi Runtime fuzz target. Decodes runtime calls from bytes, trying to achieve max coverage.
+
+use fuzz_dancelight::*;
+
+libfuzzer_sys::fuzz_target!(init: fuzz_init_only_logger(), |data: &[u8]| fuzz_decode_calls(data));
+
+/*
+libfuzzer_sys::fuzz_crossover!(|data1: &[u8], data2: &[u8], out: &mut [u8], seed: u32| {
+    fuzz_crossover_extr_or_pseudo(data1, data2, out, seed)
+});
+
+libfuzzer_sys::fuzz_mutator!(|data: &mut [u8], size: usize, max_size: usize, seed: u32| {
+    fuzz_mutator_extr_or_pseudo(data, size, max_size, seed)
+});
+*/
